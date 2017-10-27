74895d09c75b90e4a36f64d5d4a3e2ddc180c5fe
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-27 09:50:54 -0300

    creada la funcion viajesRealizados

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -35,6 +35,9 @@ class Tarjeta implements Tarjeta {
 		$fh=new DateTime();
 		$nViaje = $fh->format('Y-m-d-H-i-s');
 		$$nViaje = new Viaje ( $this , $transporte );	// El nombre del objeto tarjeta es la fecha y la hora del viaje
-		array_push ( $viajes , $$nViaje );
+		array_push ( $this->viajes , $$nViaje );
+	}
+	public function viajesRealizados () {
+		return $this->viajes;
 	}
 }
